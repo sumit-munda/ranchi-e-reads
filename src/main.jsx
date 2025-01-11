5e3b0178da38979dbf03b129fe82a098d3261772
sumit-munda <sumitkmunda.563@gmail.com>
2025-01-11 19:04:55 +0000

    Initial project setup with React, dependencies, and basic folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { FirebaseProvider } from "./context/firebase.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// implementing firebase context inside main.jsx and wrapping app component inside the FirebaseProvider so that all the app components have access to the firebase context
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<BrowserRouter>
+			<FirebaseProvider>
+				<App />
+			</FirebaseProvider>
+		</BrowserRouter>
+	</StrictMode>
+);
+
+// Our app is wrapped inside two providers...
